f6a6103cfe6fe2886f4ab3f48f4d46987e7cec80
Gustavo Passos <gustavofp111@gmail.com>
2025-06-06 17:11:11 -0300

    SCRUM-14 Tela de cadastro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { routes } from './app.routes'; // Suas rotas
+import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http'; // Importe provideHttpClient
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    // Adicione provideHttpClient() aqui para disponibilizar o HttpClient em toda a aplicação
+    provideHttpClient(withInterceptorsFromDi(), withFetch()) // withFetch() é opcional mas recomendado para novas apps
+    // Outros providers globais podem ir aqui
+  ]
 };
---
+++ src/app/core/models/user-register.model.ts
@@ -0,0 +1,10 @@
+export interface UserRegister {
+    name?: string;
+    email: string;
+    password: string;
+    username?: string;
+    phone?: string;
+    address?: string;
+    receitas?: string[];
+    favoritos?: string[];
+}
